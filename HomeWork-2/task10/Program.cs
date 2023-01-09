f100db4ad3249280a2e2c0b8f4c444e0390543b6
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-01-09 15:48:11 +0300

    решение задачи номер 10

---
+++ HomeWork-2/task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Сгенерированное число - {number}");
+Console.WriteLine($"Вторая цифра сгенерированного числа - {number/10%10}");
