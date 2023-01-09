bd06dbf0da84f6097f64888ab9fd820e4b4d91ab
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-01-09 16:02:08 +0300

    решение задачи номер 15

---
+++ HomeWork-2/task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7, где 1 - понедельник :  ");
+int number = int.Parse(Console.ReadLine());
+if (number > 7  || number < 1)
+{
+    Console.WriteLine("Число не соответствует дню недели, введите число от 1 до 7");
+}
+else
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Сегодня выходной, поэтому отдыхаем!!!");
+    }
+    else
+    {
+        Console.WriteLine("Сегодня рабочий день! Пора трудиться!");
+    }
+}
