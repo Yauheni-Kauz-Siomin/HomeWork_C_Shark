8163d820fcd2f3ea8c42a5fef316d1eb2d6ff61a
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-01-12 14:14:23 +0300

    добавил решение задачи номер 19

--- HomeWork-2/task13/Program.cs
+++ HomeWork-2/task13/Program.cs
@@ -1,19 +1,19 @@
 ﻿Console.Clear();
 Console.Write("Введите число - ");
 int number = int.Parse(Console.ReadLine());
-if (number < 0) 
+if (number < 0)
 {
     number *= -1;
 }
 if (number >= 100)
 {
-while (number > 999)
-{
-number = number / 10;
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Третья цифра числа {number % 10}");
 }
-Console.WriteLine($"Третья цифра числа {number%10}");
-}
-else 
+else
 {
     Console.Write("Введенное число не трехначное, поэтому третьей цифры нет!");
 }
