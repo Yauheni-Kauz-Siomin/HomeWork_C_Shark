dc8a60e2fc410ff58de809f3c75edf18698d8001
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-01-18 17:53:52 +0300

    Добавил два решения задачи №25

--- HomeWork_4/task25/Program.cs
+++ HomeWork_4/task25/Program.cs
@@ -16,3 +16,26 @@ for (int i = 0; i < b; i++)
 
 }
 Console.WriteLine($"Число {a} в степени {b} равняется {result}");
+
+// решение задачи без использования функции
+
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {a} в степени {b} равняется {Result(a, b)}");
+
+int Result(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+/*  решение задачи с использованием функции, при таком решении проверка 
+на равенство нулю или при вводе отрицательных чисел не нужна, все итак работает корректно */
+
