be431e79db6ce26a2c2d42927fce733eb4be2fe0
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-01-23 14:55:32 +0300

    Добавил решение задачи №34

---
+++ HomeWork_5/task34/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+
+Console.Clear();
+int[] sumEven = CreateArray();
+Console.WriteLine($"[{String.Join(", ", sumEven)}]");
+Console.WriteLine($"Количество чётных чисел в массиве составляет {SchowSumEvenNumbers(sumEven)}");
+
+
+
+int[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int SchowSumEvenNumbers(int[] arr)
+{
+    int sumEvenNumber = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) sumEvenNumber++;
+    }
+    return sumEvenNumber;
+}
+
+// решение метода SchowSumEvenNumbers с использованием операто foreach
+/* int SchowSumEvenNumbers(int[] arr)
+{
+    int sumEvenNumber = 0;
+    foreach (var item in arr)
+    {
+        if (item % 2 == 0) sumEvenNumber++;
+    }
+    return sumEvenNumber;
+} */
