d15dcad2b43dc3525864146cb483f9efc2a95f5e
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-01-30 18:07:45 +0300

    внёс последние правки в задачу номер 47

--- HomeWork_7/task47/Program.cs
+++ HomeWork_7/task47/Program.cs
@@ -19,7 +19,7 @@ double[,] GetArray(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = Convert.ToDouble(new Random().Next(0, 10)) / 10;
+            array[i, j] = Convert.ToDouble(new Random().Next(0, 100)) / 10;
         }
     }
     return array;
